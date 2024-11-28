89f50217ae0601cc6d07ff82b66dc610099be214
Jinwoo Choi <jk62362@mensakorea.org>
2024-11-29 02:09:35 +0900

    feat: 메인 클래스 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,17 @@
 package menu;
 
+import menu.domain.MenuController;
+import menu.util.InputValidator;
+import menu.util.Parser;
+import menu.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Parser parser = new Parser();
+        InputView inputView = new InputView(parser);
+        InputValidator inputValidator = new InputValidator(parser);
+        MenuController menuController = new MenuController(inputView, inputValidator);
+
+        menuController.start();
     }
 }
